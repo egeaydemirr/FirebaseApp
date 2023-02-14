58bb1248210c2f8bdc2070dbee59265495644e78
Ege Aydemir <egeaydemirr97@gmail.com>
2023-02-14 15:04:42 +0300

    initialization

--- FirebaseApp/FirebaseAppApp.swift
+++ FirebaseApp/FirebaseAppApp.swift
@@ -6,9 +6,25 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
 
 @main
 struct FirebaseAppApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+//    init(){
+//        FirebaseApp.configure()
+//    }
+    
     var body: some Scene {
         DocumentGroup(newDocument: FirebaseAppDocument()) { file in
             ContentView(document: file.$document)
