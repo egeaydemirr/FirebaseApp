bd51e6fd93c241a71aeac811035b2f9e1ca0a18e
Ege Aydemir <egeaydemirr97@gmail.com>
2023-02-14 15:08:07 +0300

    Struct Kullanici

---
+++ FirebaseApp/KullaniciModel.swift
@@ -0,0 +1,14 @@
+//
+//  KullaniciModel.swift
+//  FirebaseApp
+//
+//  Created by Ege Aydemir on 14.02.2023.
+//
+
+import Foundation
+
+struct Kullanici {
+    var id = UUID()
+    var kullaniciAdi: String
+    var adSoyad: String
+}
