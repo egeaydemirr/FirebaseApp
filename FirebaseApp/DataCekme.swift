cb8ef66e7a33ea8dba033375bc3f11d883652828
Ege Aydemir <egeaydemirr97@gmail.com>
2023-02-15 10:14:29 +0300

    DataCekme snapshot and Async

--- FirebaseApp/DataCekme.swift
+++ FirebaseApp/DataCekme.swift
@@ -17,6 +17,13 @@ class veriModeli : ObservableObject {
         database.collection("kullanicilar").getDocuments { snapshot, error in
             if error == nil{
                 //HATA ALINMADIYSA
+                if let snapshot = snapshot {
+                    DispatchQueue.main.async {
+                        self.kullanicilar = snapshot.documents.map({veriler in
+                            return Kullanici(kullaniciAdi: veriler["kullaniciAdi"] as! String , adSoyad: veriler["adSoyad"] as! String )
+                        })
+                    }
+                }
             }
             else {
                 //HATA VARSA
